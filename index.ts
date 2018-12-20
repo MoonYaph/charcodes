bb373c3e677551185cb9280c71af55197fde77ea
nick <zhangyaphet@gmail.com>
2018-12-20 17:45:55 +0800

    chore: update char codes

---
+++ index.ts
@@ -0,0 +1,212 @@
+/**
+ * 退格
+ */
+export const backSpace = 8;
+/**
+ * tab键 \t
+ */
+export const tab = 9;
+/**
+ * 换行 \n
+ */
+export const lineFeed = 10;
+/**
+ * 回车 \r
+ */
+export const carriageReturn = 13;
+export const shiftOut = 14;
+/**
+ * 空格
+ */
+export const space = 32;
+/**
+ * 感叹号
+ */
+export const exclamationMark = 33;
+/**
+ * 双引号
+ */
+export const quotationMark = 34;
+/**
+ * 井号 #
+ */
+export const numberSign = 35;
+/**
+ * 美元符号$
+ */
+export const dollarSign = 36;
+/**
+ * 百分比符号 %
+ */
+export const percentSign = 37;
+/**
+ * 与 &
+ */
+export const ampersand = 38;
+/**
+ * 单引号
+ */
+export const apostrophe = 39;
+/**
+ * 左小括号
+ */
+export const leftParenthesis = 40;
+/**
+ * 有小括号
+ */
+export const rightParenthesis = 41;
+/**
+ * 星号
+ */
+export const asterisk = 42;
+/**
+ * 加号
+ */
+export const plusSign = 43;
+/**
+ * 逗号
+ */
+export const comma = 44;
+/**
+ * 破折号
+ */
+export const dash = 45;
+/**
+ *  点
+ */
+export const dot = 46;
+/**
+ * 斜线
+ */
+export const slash = 47;
+export const digit0 = 48;
+export const digit1 = 49;
+export const digit2 = 50;
+export const digit3 = 51;
+export const digit4 = 52;
+export const digit5 = 53;
+export const digit6 = 54;
+export const digit7 = 55;
+export const digit8 = 56;
+export const digit9 = 57;
+/**
+ * 冒号
+ */
+export const colon = 58;
+/**
+ * 分号
+ */
+export const semicolon = 59;
+/**
+ * 小于号
+ */
+export const lessThan = 60;
+/**
+ * 等号
+ */
+export const equalsTo = 61;
+/**
+ * 大于号
+ */
+export const greaterThan = 62;
+/**
+ * 问号
+ */
+export const questionMark = 63;
+export const atSign = 64;
+export const uppercaseA = 65;
+export const uppercaseB = 66;
+export const uppercaseC = 67;
+export const uppercaseD = 68;
+export const uppercaseE = 69;
+export const uppercaseF = 70;
+export const uppercaseG = 71;
+export const uppercaseH = 72;
+export const uppercaseI = 73;
+export const uppercaseJ = 74;
+export const uppercaseK = 75;
+export const uppercaseL = 76;
+export const uppercaseM = 77;
+export const uppercaseN = 78;
+export const uppercaseO = 79;
+export const uppercaseP = 80;
+export const uppercaseQ = 81;
+export const uppercaseR = 82;
+export const uppercaseS = 83;
+export const uppercaseT = 84;
+export const uppercaseU = 85;
+export const uppercaseV = 86;
+export const uppercaseW = 87;
+export const uppercaseX = 88;
+export const uppercaseY = 89;
+export const uppercaseZ = 90;
+/**
+ * 左方括号
+ */
+export const leftSquareBracket = 91;
+/**
+ * 反斜线符号
+ */
+export const backslash = 92;
+/**
+ * 右方括号
+ */
+export const rightSquareBracket = 93;
+/**
+ * 插入符号 ^
+ */
+export const caret = 94;
+/**
+ * 下划线
+ */
+export const underscore = 95;
+/**
+ * 反引号
+ */
+export const graveAccent = 96;
+export const lowercaseA = 97;
+export const lowercaseB = 98;
+export const lowercaseC = 99;
+export const lowercaseD = 100;
+export const lowercaseE = 101;
+export const lowercaseF = 102;
+export const lowercaseG = 103;
+export const lowercaseH = 104;
+export const lowercaseI = 105;
+export const lowercaseJ = 106;
+export const lowercaseK = 107;
+export const lowercaseL = 108;
+export const lowercaseM = 109;
+export const lowercaseN = 110;
+export const lowercaseO = 111;
+export const lowercaseP = 112;
+export const lowercaseQ = 113;
+export const lowercaseR = 114;
+export const lowercaseS = 115;
+export const lowercaseT = 116;
+export const lowercaseU = 117;
+export const lowercaseV = 118;
+export const lowercaseW = 119;
+export const lowercaseX = 120;
+export const lowercaseY = 121;
+export const lowercaseZ = 122;
+/**
+ * 左括号
+ */
+export const leftCurlyBrace = 123;
+/**
+ * 竖线
+ */
+export const verticalBar = 124;
+/**
+ * 右括号
+ */
+export const rightCurlyBrace = 125;
+/**
+ * 波浪符号
+ */
+export const tilde = 126;
+export const nonBreakingSpace = 160;
+export const oghamSpaceMark = 5760;
+export const lineSeparator = 8232;
+export const paragraphSeparator = 8233;
